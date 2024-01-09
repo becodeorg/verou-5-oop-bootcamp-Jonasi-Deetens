bf9d45d0b03dccf86ce4f6ad1eb5ce362c88eeec
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-01-09 15:50:04 +0100

    Add classes shoppingcart and item to generate a list and totalprice

---
+++ Case 1/ShoppingCart.php
@@ -0,0 +1,34 @@
+<?php
+require_once("Item.php");
+class ShoppingCart
+{
+    private array $basket;
+
+    public function __construct()
+    {
+        $this->basket = [];
+    }
+
+    public function getBasket()
+    {
+        return $this->basket;
+    }
+
+    public function addItem(Item $item, int $amount)
+    {
+        $this->basket[] = ["item" => $item, "amount" => $amount];
+    }
+
+    public function getTotalPrice()
+    {
+        $totalPrice = 0;
+
+        foreach($this->basket as $item) {
+            $amount = $item["amount"];
+            $price = $item["item"]->getPrice();
+            $totalPrice += $amount * $price;
+        };
+
+        return $totalPrice;
+    }
+}
---
+++ Case 1/index.php
@@ -0,0 +1,57 @@
+<?php
+
+$basket = [
+    ["item" => "Banana's", "Amount" => 6, "Price" => 1, "Type" => "Fruit"],
+    ["item" => "Apples", "Amount" => 3, "Price" => 1.5, "Type" => "Fruit"],
+    ["item" => "Bottles of wine", "Amount" => 2, "Price" => 10, "Type" => "Wine"]
+];
+
+function getTotalTax($basket)
+{
+    $totalTax = 0;
+
+    foreach($basket as $item) {
+        $amount = $item['Amount'];
+        $price = $item['Price'];
+        if ($item["Type"] === "Fruit") $totalTax += $amount * $price * 0.06;
+        else $totalTax += $amount * $price * 0.21;
+    };
+
+    return $totalTax;
+}
+
+function getTotalPrice($basket)
+{
+    $totalPrice = 0;
+
+    foreach($basket as $item) {
+        $amount = $item['Amount'];
+        $price = $item['Price'];
+        $totalPrice += $amount * $price;
+    };
+
+    return $totalPrice;
+}
+
+$total = getTotalPrice($basket);
+echo "Total price: &euro;" . number_format($total, 2); 
+echo "<br>";
+
+$totalTax = getTotalTax($basket);
+echo "Total tax: &euro;" . number_format($totalTax, 2); 
+
+require_once("ShoppingCart.php");
+require_once("Item.php");
+
+$shoppingCart = new ShoppingCart();
+
+$banana = new Item("Banana", 1, "Fruit", 0.06);
+$apple = new Item("Apple", 1.5, "Fruit", 0.06);
+$wine = new Item("Wine", 10, "Fruit", 0.21);
+
+$shoppingCart->addItem($banana, 6);
+$shoppingCart->addItem($apple, 3);
+$shoppingCart->addItem($wine, 2);
+
+echo "<br>";
+echo "Total price: &euro;" . number_format($shoppingCart->getTotalPrice(), 2);
